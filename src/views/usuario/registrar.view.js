9f4f0c5c2a6486ce014a319149651a91ce4dc6d9
Isaac <48778675+Rodyu10@users.noreply.github.com>
2020-05-26 01:31:31 -0300

    Se piso el registrar

--- src/views/usuario/registrar.view.js
+++ src/views/usuario/registrar.view.js
@@ -1,19 +1,15 @@
-import React,{Component}  from 'react';
+import React, {Component} from 'react';
 import '../../assets/sass/App.scss';
-
-import DatePicker from "react-datepicker";
-import { registerLocale } from  "react-datepicker";
-
-import { ToastContainer, toast } from 'react-toastify';
+import DatePicker, {registerLocale} from "react-datepicker";
+import {toast, ToastContainer} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-
 import es from 'date-fns/locale/es';
-import { Link } from "react-router-dom";
-
-import {connect} from "react-redux";
-import {registerUser} from "../../redux/actions";
+import {Link} from "react-router-dom";
 
 import "react-datepicker/dist/react-datepicker.css";
+import {withRouter} from "react-router";
+import {connect} from "react-redux";
+import {registerUser} from "../../redux/authUser/actions";
 
 
 class Registrar extends Component {
@@ -28,7 +24,7 @@ class Registrar extends Component {
             conPass: '',
             fechaNac: new Date(),
             juegos: null,
-            errorPrinted: false
+            messagePrinted: false
         };
         registerLocale('es', es)
 
@@ -42,13 +38,14 @@ class Registrar extends Component {
         });
     }
 
-    onChange(e){
+    onChange(e) {
         this.setState({[e.target.name]: e.target.value});
     }
 
-    onSubmit(e){
+    onSubmit(e) {
+        this.setState({...this.state, messagePrinted: false});
         e.preventDefault();
-        if(this.state.password === this.state.conPass){
+        if (this.state.password === this.state.conPass) {
             const usuario = {
                 nombre: this.state.nombre,
                 apellido: this.state.apellido,
@@ -57,49 +54,49 @@ class Registrar extends Component {
                 fechaNac: this.state.fechaNac,
                 juegos: this.state.juegos
             }
-
             this.props.registerUser(usuario, this.props.history);
-            /*gamequizServices.services.registrar(usuario)
-            .then(response => {
-                console.log(response.data)
-
-                toast.success('Se ha creado el usuario correctamente');
-            }).catch(err => console.log(err))*/
         } else {
             toast.error('Las contraseñas no coinciden');
         }
     }
 
+
     componentDidUpdate() {
-        if (this.props.error && !this.state.errorPrinted) {
-            this.setState({...this.state, errorPrinted: true});
-            switch (this.props.error.message) {
-                case "Request failed with status code 404":
-                    toast.error("El usuario especificado no existe.");
-                    break;
-                case "Request failed with status code 401":
-                    toast.error("La contraseña ingresada es incorrecta.");
+        if (this.props.error !== '' && !this.state.messagePrinted) {
+            this.setState({...this.state, messagePrinted: true});
+            switch (this.props.error) {
+                case "Request failed with status code 409":
+                    toast.error("El usuario ingresado ya existe");
                     break;
                 default:
-                    toast.error(this.props.error.message);
+                    toast.error(this.props.error)
                     break;
             }
+        }
 
+        if (this.props.success_message != '' && !this.state.messagePrinted) {
+            this.setState({...this.state, messagePrinted: true});
+            toast.success(this.props.success_message);
         }
     }
 
     render() {
         return (
             <div>
-                <ToastContainer position="top-center" />
+                <ToastContainer position="top-center"/>
                 <form onSubmit={this.onSubmit}>
                     <div className="titulo-login">Registrarse</div>
-                    <input type="text" placeholder="Nombre" name="nombre" value={this.state.nombre} onChange={this.onChange} autoFocus required/>
-                    <input type="text" placeholder="Apellido" name="apellido" value={this.state.apellido} onChange={this.onChange} required/>
-                    <input type="text" placeholder="Usuario" name="username" value={this.state.usuario} onChange={this.onChange} required/>
-                    <input type="password" placeholder="Contraseña" name="password" value={this.state.pass} onChange={this.onChange} required/>
-                    <input type="password" placeholder="Repetir contraseña" name="conPass" value={this.state.conPass} onChange={this.onChange} required/>
-                    <DatePicker 
+                    <input type="text" placeholder="Nombre" name="nombre" value={this.state.nombre}
+                           onChange={this.onChange} autoFocus required/>
+                    <input type="text" placeholder="Apellido" name="apellido" value={this.state.apellido}
+                           onChange={this.onChange} required/>
+                    <input type="text" placeholder="Usuario" name="username" value={this.state.usuario}
+                           onChange={this.onChange} required/>
+                    <input type="password" placeholder="Contraseña" name="password" value={this.state.pass}
+                           onChange={this.onChange} required/>
+                    <input type="password" placeholder="Repetir contraseña" name="conPass" value={this.state.conPass}
+                           onChange={this.onChange} required/>
+                    <DatePicker
                         className="m-0"
                         dateFormat="dd/MM/yyyy"
                         placeholderText="Fecha de Nacimiento"
@@ -121,8 +118,8 @@ class Registrar extends Component {
 }
 
 const mapStateToProps = ({authUser}) => {
-    const {user, error} = authUser;
-    return {user, error};
+    const {success_message, error} = authUser;
+    return {success_message, error};
 };
 
-export default connect(mapStateToProps, {registerUser})(Registrar);
+export default withRouter(connect(mapStateToProps, {registerUser})(Registrar));
