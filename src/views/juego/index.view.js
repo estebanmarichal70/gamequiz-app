99b579452bdbc54bb5b355989e691a3bad724240
Caste <gabrielcastellano263@gmail.com>
2020-05-28 17:18:56 -0300

    merge con iniciar juego responsive

--- src/views/juego/index.view.js
+++ src/views/juego/index.view.js
@@ -11,6 +11,7 @@ const Empezar = React.lazy(() => import("./empezar.view"));
 const Jugando = React.lazy(() => import("./jugando.view"));
 const Youtube = React.lazy(() => import("./youtube.view"));
 const Join = React.lazy(() => import("./join.view"));
+const Inicio = React.lazy(() => import("./inicioJuego.view"));
 
 const Juego = ({match}) => {
     return (
@@ -52,6 +53,11 @@ const Juego = ({match}) => {
                 <Route
                     path={`${match.url}/join`}
                     render={(props) => <Join {...props} />}
+                    exact
+                />
+                <Route
+                    path={`${match.url}/inicio`}
+                    render={(props) => <Inicio {...props} />}
                     exact
                 />
             </Switch>
